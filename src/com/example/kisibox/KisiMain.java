f147fa745bb34e15c15e561c9e588094530b45ea
Andreas Hubel <andreas.hubel@tum.de>
2013-07-06 22:39:57 +0200

    connect mainscreen to api

--- src/com/example/kisibox/KisiMain.java
+++ src/com/example/kisibox/KisiMain.java
@@ -3,16 +3,25 @@ package com.example.kisibox;
 import java.util.List;
 import java.util.Vector;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.example.kisibox.model.Location;
+import com.manavo.rest.RestCallback;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.util.SparseArray;
 import android.view.Window;
 import android.widget.TextView;
 
+
 public class KisiMain extends FragmentActivity {
 
+	public SparseArray<Location> locations;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
@@ -32,23 +41,43 @@ public class KisiMain extends FragmentActivity {
 	}
 
 	public void initializePager() {
-		//TODO: change from Test to real Data
+		updateLocations();
+	}
 
+	private void updateLocations() {
+		KisiApi api = new KisiApi(this);
 
+		api.setCallback(new RestCallback() {
+			public void success(Object obj) {
+				JSONArray data = (JSONArray)obj;
+
+				setupView(data);
+			}
+
+		});
+		api.get("locations");
+		
+	}
+	
+	private void setupView(JSONArray locations_json) {
+		
 		List<Fragment> fragments = new Vector<Fragment>(); 
-		fragments.add(LocationOneDoor.newInstance("Home","Römerstraße","17","Front Door"));
+		locations = new SparseArray<Location>();
 		
-		
-		fragments.add(LocationTwoDoors.newInstance("Meier","Hauptstraße","5","Front Door","Appartment Door"));
-		fragments.add(LocationThreeDoors.newInstance("Müller","Dorfstraße","23","Front Door","Garage Door","Cellar Door"));	
-		fragments.add(LocationFourDoors.newInstance("Uni","Boltzmannstraße","15","Front Door","Library","Room 02.07.23","Room 00.09.11"));		
-		
-		
+		try {
+			for (int i=0; i<locations_json.length(); i++) {
+				Location location = new Location(locations_json.getJSONObject(i));
+				locations.put(location.getId(), location);
+				fragments.add(LocationViewFragment.newInstance(location.getId()));
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		FragmentManager fm = getSupportFragmentManager();
 		ViewPager pager = (ViewPager) findViewById(R.id.pager);
-		MyFragmentPagerAdapter pagerAdapter = new MyFragmentPagerAdapter(fm,
-				fragments);
+		MyFragmentPagerAdapter pagerAdapter = new MyFragmentPagerAdapter(fm, fragments);
 		pager.setAdapter(pagerAdapter);
-
 	}
 }
