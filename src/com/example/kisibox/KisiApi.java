827795010ab0477de65f8103380879cdbbef74fc
Andreas Hubel <andreas.hubel@tum.de>
2013-07-10 17:46:19 +0200

    fixed login

--- src/com/example/kisibox/KisiApi.java
+++ src/com/example/kisibox/KisiApi.java
@@ -1,37 +1,48 @@
 package com.example.kisibox;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.manavo.rest.RestApi;
+
 import android.app.Activity;
-import android.content.SharedPreferences;
-import android.provider.Settings;
-import android.util.Log;
+
 
 
 public class KisiApi extends RestApi {
+	
+	private static String authToken;
 
 	public KisiApi(Activity activity) {
 		super(activity);
-		
-		SharedPreferences settings = getSharedPreferences("Config", MODE_PRIVATE);
-		String authToken = settings.getString("authToken", "");
 
-		this.BASE_URL = "https://kisi.de/";
+		this.BASE_URL = "https://www.kisi.de/";
 		this.urlSuffix = ".json" + ( KisiApi.authToken != null ? "?auth_token=" + KisiApi.authToken : "" );
-		this.rest.setHost("kisi.de");
-		//this.rest.setPort(80);
+		this.rest.setHost("www.kisi.de");
+		this.rest.setPort(443);
 		this.setUserAgent("AKey");
+		this.rest.setContentType("application/json");
+	}
 
-		this.acceptAllSslCertificates();
 	public static void setAuthToken(String token) {
 		KisiApi.authToken = token;
 	}
+	
 	// return class instance so we can chain calls
 	public KisiApi authorize(String email, String password) {
-		//addParameter("user", JSONObject("{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}"));
-		addParameter("user", "{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}");
+		try {
+			JSONObject data = new JSONObject();
+			data.put("email", email);
+			data.put("password", password);
+			this.addParameter("user", (Object) data);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 		return this;
 	}
-	
+
 	@Override // to make method public
 	public void get(String url) {
 		super.get(url);
