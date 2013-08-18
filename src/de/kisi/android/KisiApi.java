f48e262ad9021eadf1a84a8dc685cc3a2ab1f1b9
Andreas Hubel <andreas.hubel@tum.de>
2013-08-18 21:08:44 +0200

    redirect to login if token is invalid

--- src/de/kisi/android/KisiApi.java
+++ src/de/kisi/android/KisiApi.java
@@ -6,6 +6,9 @@ import org.json.JSONObject;
 import com.manavo.rest.RestApi;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Toast;
 
 
 
@@ -65,6 +68,12 @@ public class KisiApi extends RestApi {
 	
 	@Override 
 	public void onStatusCodeError(int code, String data) {
+		if (code == 401) {
+			Toast.makeText(activity, R.string.session_timeout_relogin, Toast.LENGTH_LONG).show();
+			Intent loginScreen = new Intent(activity.getApplicationContext(), LoginActivity.class);
+			activity.startActivity(loginScreen);
+			return;
+		}
 		if (this.errorCallback == null && data.startsWith("{\"error\":") ) {
 			try {
 				JSONObject data_json = new JSONObject(data);
